e02e2da96e74b3644c1181f17dc996e70ef17750
Taras Murzenkov <taras.murzenkov@freelancers.outfittery.de>
2022-05-08 19:10:46 +0200

    initial commit

--- src/main/java/de/outifttery/Main.java
+++ src/main/java/de/outifttery/Main.java
@@ -1,12 +1,12 @@
 package de.outifttery;
 
+import de.outifttery.model.deployment.Container;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import static de.outifttery.DeploymentActions.updateImageVersion;
 import static de.outifttery.K8Manager.withK8SClient;
 import static de.outifttery.model.deployment.DeploymentDescription.deployment;
-import static de.outifttery.model.deployment.Container.image;
 import static io.kubernetes.client.util.ClientBuilder.standard;
 
 @Slf4j
@@ -17,7 +17,7 @@ public class Main {
         withK8SClient(standard().build())
                 .addActionToPipeline(
                         updateImageVersion(
-                                image().imageName("nginx").imageTag("1.14.2").build(),
+                                Container.image().imageName("nginx").imageTag("1.14.2").build(),
                                 deployment().deploymentName("nginx-deployment").build()
                         ))
                 .runActions()
